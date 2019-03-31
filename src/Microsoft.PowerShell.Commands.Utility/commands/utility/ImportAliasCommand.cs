58d46be5f412d23f713d4452419f849de907f7b6
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2019-03-31 21:42:05 +0200

    moved method from csv.cs to ImportAliasCommand.cs

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/ImportAliasCommand.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/ImportAliasCommand.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Management.Automation;
 using System.Management.Automation.Internal;
 using System.Security;
+using System.Text;
 
 namespace Microsoft.PowerShell.Commands
 {
@@ -293,7 +294,7 @@ namespace Microsoft.PowerShell.Commands
             string filePath = null;
             using (StreamReader reader = OpenFile(out filePath, isLiteralPath))
             {
-                CSVHelper csvHelper = new CSVHelper(',');
+                //CSVHelper csvHelper = new CSVHelper(',');
 
                 Int64 lineNumber = 0;
                 string line = null;
@@ -302,7 +303,7 @@ namespace Microsoft.PowerShell.Commands
                     ++lineNumber;
 
                     // Ignore blank lines
-                    if (line.Length == 0)
+                    if (line.Length == 0) 
                     {
                         continue;
                     }
@@ -319,7 +320,7 @@ namespace Microsoft.PowerShell.Commands
                         continue;
                     }
 
-                    Collection<string> values = csvHelper.ParseCsv(line);
+                    Collection<string> values = ParseCsvLine(line);
 
                     if (values.Count != 4)
                     {
@@ -379,8 +380,77 @@ namespace Microsoft.PowerShell.Commands
 
                 reader.Dispose();
             }
-
+            //return null;
             return result;
+        }
+
+        private Collection<string> ParseCsvLine(string csv)
+        {        
+            csv = csv.Trim();
+            Collection<string> result = new Collection<string>();
+            if (csv.Length == 0 || csv[0] == '#')
+            {
+                return result;
+            }
+
+            var reader = new StringReader(csv);
+            StringBuilder wordBuffer = new StringBuilder();
+
+            while (reader.Peek() != -1) 
+            {
+                char nextChar = (char)reader.Read();
+
+                // if next character was delimiter or we are at the end, add string to result and clear wordBuffer
+                // else if next character was quote, perform reading until next quote and add it to wordBuffer
+                // else read and add it to wordBuffer
+                if (nextChar == ',') 
+                {
+                    result.Add(wordBuffer.ToString());
+                    wordBuffer.Clear();
+                } 
+                else if (nextChar == '"') 
+                {
+                    bool inQuotes = true;
+                    
+                    // if we are within a quote section, read and append to wordBuffer until we find a next quote that is not followed by another quote
+                    // if it is a single quote, escape the quote section
+                    // if the quote is followed by an other quote, do not escape and add a quote character to wordBuffer
+                    while (reader.Peek() != -1 && inQuotes) 
+                    {
+                        nextChar = (char)reader.Read();
+                        
+                        if (nextChar == '"') 
+                        {
+                            if ((char)reader.Peek() == '"')
+                            {
+                                wordBuffer.Append(nextChar);
+                                reader.Read();
+                            } 
+                            else 
+                            {
+                                inQuotes = false;
+                            }
+                        } 
+                        else 
+                        {
+                            wordBuffer.Append(nextChar);
+                        }
+                    }
+                } 
+                else 
+                {
+                    wordBuffer.Append(nextChar);
+                }
+            }
+
+            string lastWord = wordBuffer.ToString();
+            if (lastWord != string.Empty)
+            {
+                result.Add(lastWord);
+            }
+
+            reader.Close();    
+            return result;           
         }
 
         private StreamReader OpenFile(out string filePath, bool isLiteralPath)
@@ -479,4 +549,6 @@ namespace Microsoft.PowerShell.Commands
         }
         #endregion Command code
     }
+
+    
 }
